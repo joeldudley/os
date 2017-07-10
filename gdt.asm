41c901e51b1b4b9dc216f9dbf2df72e921729d7e
joeldudleyr3 <joeldudley@outlook.com>
2017-07-10 23:24:23 +0100

    Improves commenting of GDT file.

--- gdt.asm
+++ gdt.asm
@@ -1,36 +1,37 @@
 [bits 16]
-gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
-    ; the GDT starts with a null 8-byte
-    dd 0x0 ; 4 byte
-    dd 0x0 ; 4 byte
+gdt_start:
+    dd 0x0                          ; The GDT starts with a null 8-byte.
+    dd 0x0                          ;   "
 
-; GDT for code segment. base = 0x00000000, length = 0xfffff
-; for flags, refer to os-dev.pdf document, page 36
-gdt_code: 
-    dw 0xffff    ; segment length, bits 0-15
-    dw 0x0       ; segment base, bits 0-15
-    db 0x0       ; segment base, bits 16-23
-    db 10011010b ; flags (8 bits)
-    db 11001111b ; flags (4 bits) + segment length, bits 16-19
-    db 0x0       ; segment base, bits 24-31
+gdt_code:                           ; GDT for the code segment.
+; Base = 0x0, limit = 0xfffff.
+; 1st set of flags: present = 1, privilege = 00, descriptor type = 1.
+; Type flags: code = 1, conforming = 0, readable = 1, accessed = 0.
+; 2nd set of flags: granularity = 1, 32-bit default = 1, 64-bit seg = 0, AVL = 0.
+    dw 0xffff                       ; Limit, bits 0-15.
+    dw 0x0                          ; Base, bits 0-15.
+    db 0x0                          ; Base, bits 16-23.
+    db 10011010b                    ; First set of flags & type flags.
+    db 11001111b                    ; Second set of flags & limit (bits 16-19).
+    db 0x0                          ; Base, bits 24-31.
 
-; GDT for data segment. base and length identical to code segment
-; some flags changed, again, refer to os-dev.pdf
-gdt_data:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10010010b
-    db 11001111b
-    db 0x0
+gdt_data:                           ; GDT for the data segment.
+; Base = 0x0, limit = 0xfffff.
+; 1st set of flags: present = 1, privilege = 00, descriptor type = 1.
+; Type flags: code = 0, expand down = 0, writable = 1, accessed = 0.
+; 2nd set of flags: granularity = 1, 32-bit default = 1, 64-bit seg = 0, AVL = 0.
+    dw 0xffff                       ; Limit, bits 0-15.
+    dw 0x0                          ; Base, bits 0-15.
+    db 0x0                          ; Base, bits 16-23.
+    db 10010010b                    ; First set of flags & type flags.
+    db 11001111b                    ; Second set of flags & limit (bits 16-19).
+    db 0x0                          ; Base, bits 24-31.
 
-gdt_end:
+gdt_end:                            ; Can I remove this, and replace `gdt_end` with `$`?
 
-; GDT descriptor
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
-    dd gdt_start ; address (32 bit)
+gdt_descriptor:                     ; GDT descriptor.
+    dw gdt_end - gdt_start - 1      ; Size (16 bit), always one less than its true size.
+    dd gdt_start                    ; Address (32 bit)
 
-; define some constants for later use
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: makeos
 
 makeos:
-	nasm -f bin os.asm -o os.bin
-	qemu-system-i386 os.bin
+	nasm -f bin main.asm -o main.bin
+	qemu-system-i386 main.bin
