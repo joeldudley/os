954c445b911f8dd94b6822ab01f656d5212a7a30
joeldudleyr3 <joeldudley@outlook.com>
2017-07-12 10:15:44 +0100

    Improves comments.

--- kernel/main.c
+++ kernel/main.c
@@ -1,32 +1,32 @@
 #include "ports.h"
 
+/** 
+ * Writes 'X' to the cursor's current position. 
+ */
 void main() {
-    /* Screen cursor position: ask VGA control register (0x3d4) for bytes
-     * 14 = high byte of cursor and 15 = low byte of cursor. */
-    port_byte_out(0x3d4, 14); /* Requesting byte 14: high byte of cursor pos */
-    /* Data is returned in VGA data register (0x3d5) */
-    int position = port_byte_in(0x3d5);
-    position = position << 8; /* high byte */
+    int cursor_pos;
+    // We request the high byte of the cursor (14) from the VGA control 
+    // register (0x3d4).
+    port_byte_out(0x3d4, 14);
+    // We receive the high byte of the cursor via the VGA data register 
+    // (0x3d5).
+    cursor_pos = port_byte_in(0x3d5);
 
-    port_byte_out(0x3d4, 15); /* requesting low byte */
-    position += port_byte_in(0x3d5);
+    // We left-shift this value because it's the high byte.
+    cursor_pos = cursor_pos << 8;
 
-    /* VGA 'cells' consist of the character and its control data
-     * e.g. 'white on black background', 'red text on white bg', etc */
-    int offset_from_vga = position * 2;
+    // We repeat the process for the low byte of the cursor (15).
+    port_byte_out(0x3d4, 15);
+    cursor_pos += port_byte_in(0x3d5);
 
-    /* Now you can examine both variables using gdb, since we still
-     * don't know how to print strings on screen. Run 'make debug' and
-     * on the gdb console:
-     * breakpoint kernel.c:21
-     * continue
-     * print position
-     * print offset_from_vga
-     */
+    // Each VGA 'cell' pairs the character and its control data (e.g. 
+    // background and foreground color). We double the cursor's position to 
+    // find the VGA cell's location.
+    int offset_from_vga = cursor_pos * 2;
 
-    /* Let's write on the current cursor position, we already know how
-     * to do that */
+    // Writing 'X' to the current cursor position.
     char *vga = 0xb8000;
-    vga[offset_from_vga] = 'X'; 
-    vga[offset_from_vga+1] = 0x0f; /* White text on black background */
+    vga[offset_from_vga] = 'X';
+    // White text on a black background.
+    vga[offset_from_vga+1] = 0x0f;
 }
--- kernel/ports.c
+++ kernel/ports.c
@@ -12,27 +12,28 @@
     * The sections are separated by colons:
         * The middle section assigns the ASM value to the C variable in brackets
         * The final section passes the C variable in brackets to ASM
-*/
-
-/**
- * Read a byte from the specified port
  */
+
+/** Read a byte from the specified port. */
 unsigned char port_byte_in (unsigned short port) {
     unsigned char result;
     __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
 
+/** Write a byte to the specified port. */
 void port_byte_out (unsigned short port, unsigned char data) {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
+/** Read a word from the specified port. */
 unsigned short port_word_in (unsigned short port) {
     unsigned short result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
 }
 
+/** Write a word to the specified port. */
 void port_word_out (unsigned short port, unsigned short data) {
     __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
