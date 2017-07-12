bbbf99d31eb75141410915f805a614bd78a1fb31
joeldudleyr3 <joeldudley@outlook.com>
2017-07-12 10:17:25 +0100

    Reorders functions.

--- kernel/ports.c
+++ kernel/ports.c
@@ -14,6 +14,16 @@
         * The final section passes the C variable in brackets to ASM
  */
 
+/** Write a byte to the specified port. */
+void port_byte_out (unsigned short port, unsigned char data) {
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+/** Write a word to the specified port. */
+void port_word_out (unsigned short port, unsigned short data) {
+    __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
+
 /** Read a byte from the specified port. */
 unsigned char port_byte_in (unsigned short port) {
     unsigned char result;
@@ -21,19 +31,9 @@ unsigned char port_byte_in (unsigned short port) {
     return result;
 }
 
-/** Write a byte to the specified port. */
-void port_byte_out (unsigned short port, unsigned char data) {
-    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
-}
-
 /** Read a word from the specified port. */
 unsigned short port_word_in (unsigned short port) {
     unsigned short result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
-}
-
-/** Write a word to the specified port. */
-void port_word_out (unsigned short port, unsigned short data) {
-    __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
