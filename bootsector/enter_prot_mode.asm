1d30a02f80f8be0edd05519925727442b8d4b881
joeldudleyr3 <joeldudley@outlook.com>
2017-08-05 16:18:10 +0100

    Adds comments.

--- bootsector/enter_prot_mode.asm
+++ bootsector/enter_prot_mode.asm
@@ -2,10 +2,12 @@
 enter_prot_mode:                    ; Enters protected mode.
     cli                             ; Disables interrupts.
     lgdt [gdt_descriptor]           ; Loads the global table descriptor.
-    mov eax, cr0                    ; Set the first bit of cr0. ??? WHY? ???
-    or eax, 0x1                     ;   "
-    mov cr0, eax                    ;   "
-    jmp CODE_SEG:init_prot_mode     ; A far-jump flushing the CPU pipeline.
+    mov eax, cr0                    ; We make the switch the protected mode by setting the first 
+    or eax, 0x1                     ; bit of control register CR0, without modifying the existing 
+    mov cr0, eax                    ; bits.
+    jmp CODE_SEG:init_prot_mode     ; Far-jumping forces the CPU to flush any remaining 16-bit 
+                                    ; operations in its pipeline, and updates `cs` to point to 
+                                    ; the code segment of the GDT.
 
 [bits 32]
 init_prot_mode:                     ; Initialises protected mode.
