ca803a8c96124caac34010900a2540ccc498f7eb
joeldudleyr3 <joeldudley@outlook.com>
2017-08-05 19:06:15 +0100

    Uses generic types.

--- kernel/ports.h
+++ kernel/ports.h
@@ -5,7 +5,7 @@
 
 void port_write_byte(u16 port, u8 data);
 void port_write_word(u16 port, u16 data);
-unsigned char port_read_byte(u16 port);
-unsigned short port_read_word(u16 port);
+u8 port_read_byte(u16 port);
+u16 port_read_word(u16 port);
 
 #endif
